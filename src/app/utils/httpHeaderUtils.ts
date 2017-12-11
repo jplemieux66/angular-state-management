19429d15f670b9f573cc082bb57fa433dd1dd28d
JP <phillemieux66@hotmail.com>
2017-12-11 17:17:56 -0500

    Fix createCommonHeaders function, had a bug because HttpHeaders is immutable

--- src/app/utils/httpHeaderUtils.ts
+++ src/app/utils/httpHeaderUtils.ts
@@ -1,17 +1,15 @@
-import { Headers } from "@angular/http";
+import { HttpHeaders } from "@angular/common/http";
 
-export function addUserIdHeader(headers: Headers, userId: number): Headers {
-  headers.append('USERID', userId.toString());
-  return headers;
+export function addUserIdHeader(HttpHeaders: HttpHeaders, userId: number): HttpHeaders {
+  return HttpHeaders.append('userid', userId.toString());
 }
 
-export function addJsonContentTypeHeader(headers: Headers): Headers {
-  headers.append('Content-Type', 'application/json; charset=utf-8')
-  return headers;
+export function addJsonContentTypeHeader(HttpHeaders: HttpHeaders): HttpHeaders {
+  return HttpHeaders.append('Content-Type', 'application/json; charset=utf-8')
 }
 
-export function createCommonHeaders(userId: number): Headers {
-  let headers = new Headers();
+export function createCommonHeaders(userId: number): HttpHeaders {
+  let headers = new HttpHeaders();
   headers = addUserIdHeader(headers, userId);
   headers = addJsonContentTypeHeader(headers);
   return headers;
