6136767ec4d16b26455c4a1b00b5d572ae89fd2c
JP <phillemieux66@hotmail.com>
2017-12-16 17:44:12 -0500

    Add mergeMap rxjs operator

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ import { environment } from './environments/environment';
 
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/switchMap';
+import 'rxjs/add/operator/mergeMap';
 
 if (environment.production) {
   enableProdMode();
