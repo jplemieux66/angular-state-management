21cb53245d67493480816bca51da3905c05d2f38
JP <phillemieux66@hotmail.com>
2017-12-19 07:55:47 -0500

    Bug FIx (api): Types expected were wrong

--- src/server/persistence/addTimeEntry.ts
+++ src/server/persistence/addTimeEntry.ts
@@ -1,14 +1,15 @@
 import { Moment } from "moment";
+import * as moment from 'moment';
 import { dbTimeEntries } from "../db-data";
 import { TimeEntry } from "../../shared/model/time-entry";
 
-export function addTimeEntry(date: Moment, projectId: number, userId: number, time: number): TimeEntry {
+export function addTimeEntry(date: string, projectId: number, userId: number, time: number): TimeEntry {
   let id: number = Object.keys(dbTimeEntries).reduce(function(a, b){ return a > parseInt(b) ? a : parseInt(b) }, 0);
   id++;
-
+  
   const newTimeEntry = {
     id,
-    date: date.toDate(),
+    date: moment(date).toDate(),
     projectId,
     userId,
     time
